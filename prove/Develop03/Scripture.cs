7c77d136d35ffb209e8d74b31cfbbca3cb3bdfb3
KukkikD <adeezon9@gmail.com>
2023-10-22 00:41:01 +0700

    update

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -3,14 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.IO;
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 class Scripture
 {
-    public string Reference { get; }
-    public string Text { get; }
+    private string reference;
+    private string text;
+    private List<string> words;
+    private int hiddenWordIndex;
 
     public Scripture(string reference, string text)
     {
-        Reference = reference;
-        Text = text;
+        this.reference = reference;
+        this.text = text;
+        this.words = text.Split(' ').ToList();
+        this.hiddenWordIndex = 0; // Start with the first word
+    }
+
+    public void Display()
+    {
+        Console.Clear();
+        Console.WriteLine(reference);
+
+        // Replace words with "___" for all words up to hiddenWordIndex
+        for (int i = 0; i < words.Count; i++)
+        {
+            if (i < hiddenWordIndex)
+                Console.Write("___ ");
+            else
+                Console.Write(words[i] + " ");
+        }
+        Console.WriteLine(); // Move to the next line
+    }
+
+    public void HideNextWord()
+    {
+        if (hiddenWordIndex < words.Count)
+        {
+            hiddenWordIndex++;
+            Display();
+        }
+    }
+
+    public bool AllWordsHidden()
+    {
+        return hiddenWordIndex == words.Count;
     }
 }
